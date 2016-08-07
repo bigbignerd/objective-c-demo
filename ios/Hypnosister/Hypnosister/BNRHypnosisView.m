90e30eb3efb4e911398c7771f1b4d5a4562800ca
bignerd <wanguohui@yahoo.com>
2016-08-08 07:27:20 +0800

    sync

--- ios/Hypnosister/Hypnosister/BNRHypnosisView.m
+++ ios/Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -8,6 +8,12 @@
 
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisView()
+
+@property (strong, nonatomic) UIColor *circleColor;
+
+@end
+
 @implementation BNRHypnosisView
 
 - (void)drawRect:(CGRect)rect
@@ -17,12 +23,23 @@
     center.x = bounds.origin.x + bounds.size.width/2.0;
     center.y = bounds.origin.y + bounds.size.height/2.0;
     //radius
-    float radius = (MIN(bounds.size.width, bounds.size.height)/2.0);
+//    float radius = (MIN(bounds.size.width, bounds.size.height)/2.0);
+    float maxRadius = hypotf(bounds.size.width, bounds.size.height) / 2.0;
     UIBezierPath *path = [[UIBezierPath alloc] init];
-    [path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
+    for(float curRadius = maxRadius;curRadius>0;curRadius -= 20){
+//        [[path moveToPoint:CGPointMake(center.x + curRadius, center.y)];
+        [path moveToPoint:CGPointMake(center.x + curRadius, center.y)];
+        [path addArcWithCenter:center radius:curRadius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
+    }
+    
+   
     path.lineWidth = 10;
-    [[UIColor lightGrayColor] setStroke];
+//    [[UIColor lightGrayColor] setStroke];
+    [self.circleColor setStroke];
     [path stroke];
+    //change draw image
+    UIImage *logoImage = [UIImage imageNamed:@"logo.png"];
+//    [logoImage drawInRect:rect];
 }
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -30,7 +47,31 @@
     if (self) {
         // All BNRHypnosisViews start with a clear background color
         self.backgroundColor = [UIColor clearColor];
+        self.circleColor = [UIColor lightGrayColor];
     }
     return self;
 }
+// When a finger touches the screen
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"%@ was touched", self);
+    
+    // Get 3 random numbers between 0 and 1
+    float red = (arc4random() % 100) / 100.0;
+    float green = (arc4random() % 100) / 100.0;
+    float blue = (arc4random() % 100) / 100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red
+                                           green:green
+                                            blue:blue
+                                           alpha:1.0];
+    
+    self.circleColor = randomColor;
+}
+- (void)setCircleColor:(UIColor *)circleColor
+{
+    _circleColor = circleColor;
+    [self setNeedsDisplay];
+}
+
 @end
